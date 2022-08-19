1e65d9cb535dd2dbfa9a4520a7e541d96178a4ed
Roodootoo <elena@pyzhov.ru>
2022-08-19 09:37:37 +0300

    Update create_employees.sql

--- create_employees.sql
+++ create_employees.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS Departments (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL
+	name VARCHAR(40) unique NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Employees (
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(60) NOT null,
+	name VARCHAR(60) unique NOT null,
 	Department_id INTEGER references Departments(id)
 );
 
